6ea017ea4621610ad1cd5ee52b729bf5ddca42be
Your Name <you@example.com>
2020-07-26 13:38:43 +0600

    first

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <title>簡単なファイル移動するアプリ</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+
+<body>
+<div class="col-md-8">
+    <?php
+        if(isset($_SESSION['message'])){?>
+            <h3 class="text-center" style="color: green"><?php echo $_SESSION['message']?></h3>
+        <?php
+        unset($_SESSION['message']);
+        }
+    ?>
+    <div class="my-box">
+        <h3 class="title text-center">簡単なファイル移動するアプリ</h3>
+        <form action="process.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="source_file">Select Source:</label>
+                <input class="form-control" type="file" name="files[]" id="files" multiple="" directory="" webkitdirectory="" mozdirectory="">
+            </div>
+            <div class="form-group">
+                <label for="destination_file">Select Destination:</label>
+                <input type="file" name="destination_file[]" class="form-control" placeholder="Select Destination file" id="destination_file" required webkitdirectory directory multiple/>
+            </div>
+                <input type="hidden" name="main_destination" id="main_destination">
+            <div class="form-group">
+                <label for="file_extension">Select File Extension:</label>
+                <select name="file_extension" id="file_extension" class="form-control">
+                    <option id="all">All</option>
+                    <option id="mp4">mp4</option>
+                    <option id="mp3">mp3</option>
+                    <option id="jpeg">jpeg</option>
+                    <option id="jpg">jpg</option>
+                    <option id="png">png</option>
+                    <option id="pdf">pdf</option>
+                    <option id="bmp">bmp</option>
+                </select>
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+<script type="text/javascript">
+    $('#destination_file').change(function () {
+        var filePath = $(this).val();
+        console.log(filePath);
+    });
+</script>
+
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+/**
+ * Created by PhpStorm.
+ * User: shuav
+ * Date: 7/22/2020
+ * Time: 11:19 AM
+ */
+if (isset($_POST['submit'])){
+    $count = 0;
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        foreach ($_FILES['files']['type'] as $i => $file) {
+            $extension = explode('/',$file);
+            $file_extension = trim($_POST['file_extension']);
+            if($file_extension == 'all'){
+                if (strlen($_FILES['files']['name'][$i]) > 1) {
+                    if (move_uploaded_file($_FILES['files']['tmp_name'][$i], 'moved_files/'.$_FILES['files']['name'][$i])) {
+                        $count++;
+                    }
+                }
+            }
+            elseif($extension[1] == $file_extension){
+                if (strlen($_FILES['files']['name'][$i]) > 1) {
+                    if (move_uploaded_file($_FILES['files']['tmp_name'][$i], 'moved_files/'.$_FILES['files']['name'][$i])) {
+                        $count++;
+                    }
+                }
+            }
+        }
+    }
+    $_SESSION['message'] = $count.' files have been moved to folder';
+    header('location:index.php');
+}
